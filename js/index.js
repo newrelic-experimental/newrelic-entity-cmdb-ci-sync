4bf085d95318414fcd33fbeb41aa328421f71f4f
Gil Rice <gil@newrelic.com>
2021-01-04 14:29:57 -0500

    readding cron config

--- js/index.js
+++ js/index.js
@@ -6,14 +6,14 @@ const config = require('./config.json');
 let cycleSync = require("./entitysync").cycleSync;
 
 
-//app = express();
-//app.get('/', (req, res) => res.send('SNOW ENTITY SYNC'));
+app = express();
+app.get('/', (req, res) => res.send('SNOW ENTITY SYNC'));
 
 // execute the daily run of the entity sync for each entity type/domain
-//cron.schedule("0 " +  config.daily_sync_time + " * * *", function() {
+cron.schedule("0 " +  config.daily_sync_time + " * * *", function() {
 
     console.log("running cycle");
     cycleSync(config);
-//});
+});
 
-//app.listen(config.express_port);
+app.listen(config.express_port);
